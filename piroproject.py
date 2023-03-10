846db61d5f6be2bacf44fcaec778dc211d9ac10b
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2023-03-10 14:57:27 +0545

    Update piroproject.py

--- piroproject.py
+++ piroproject.py
@@ -1,12 +1,12 @@
-import pyttsx3
-import speech_recognition as sr
-import webbrowser
-import datetime
-import pyjokes
+import pyttsx3 # for text to speech conversion
+import speech_recognition as sr # recognizing speech
+import webbrowser # working with browsers
+import datetime # datetime module
+import pyjokes # some lame jokes
 
 
 def sptext():
-    recognizer = sr.Recognizer()
+    recognizer = sr.Recognizer() # initiating recognizer
     with sr.Microphone() as source:  # init Microphone
         print('Listening ...')
         recognizer.adjust_for_ambient_noise(source)  # noise cancellation
@@ -22,24 +22,24 @@ def sptext():
 
 
 def speechtx(x):  # text to speech
-    engine = pyttsx3.init()
-    voices = engine.getProperty('voices')
+    engine = pyttsx3.init() # initializing pyttsx3
+    voices = engine.getProperty('voices') # get voices
     engine.setProperty('voice', voices[0].id)  # use voices[1] for female voice
-    rate = engine.getProperty('rate')
-    engine.setProperty('rate', 150)
+    rate = engine.getProperty('rate') # get the playback speed
+    engine.setProperty('rate', 150) # increase or decrease playback speed by changing the value
     engine.say(x)
     engine.runAndWait()
 
 
 if __name__ == '__main__':
-    if 'hey mango' in sptext().lower():
+    if 'hey mango' in sptext().lower(): # activating assistant
 
-        while True:
-            data1 = sptext().lower()
+        while True: # work infintely until you say exit
+            data1 = sptext().lower() # lowers all the text you have spoken
 
-            if "your name" in data1:
-                name = "my name is mango"
-                speechtx(name)
+            if "your name" in data1: # checking if "your name" is in your speech
+                name = "my name is mango" # storing your answer
+                speechtx(name) # speaking your answer
 
             elif "old are you" in data1:
                 age = "I don't have a age as I am not physical being"
@@ -56,9 +56,9 @@ if __name__ == '__main__':
                 spjoke = pyjokes.get_joke(language="en", category="neutral")
                 speechtx(spjoke)
 
-            elif "exit" in data1:
-                speechtx("Exiting. Thank you for using")
-                break
+            elif "exit" in data1: # to exit the code
+                speechtx("Exiting. Thank you for using") # exiting message
+                break # cut the loop
 
     else:
         speechtx("Thanks")
